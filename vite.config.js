250359ef7e49c209a2b6892f02791ccd89ea2874
Ole <dearraztqldx@gmail.com>
2025-08-22 12:01:03 +0800

    Fix GitHub Actions deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/vue/' : '/',
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   server: {
     port: 3000,
     open: true
